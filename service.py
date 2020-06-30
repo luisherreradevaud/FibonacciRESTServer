036babc3d6a1c0a61a2cf78960cf37ed20dc59f2
luisherreradevaud <lucho.herrera@gmail.com>
2020-06-30 17:55:02 -0400

    Update service.py

--- service.py
+++ service.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from classes import Phi
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Phi, '/phi/<n>')
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET')
+    return response
 
 if __name__ == '__main__':
      app.run(port='5001')
