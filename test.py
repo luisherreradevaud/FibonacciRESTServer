42841401b4c712bf98dac751b7c06ba3397cbfcf
luisherreradevaud <lucho.herrera@gmail.com>
2020-09-16 13:32:04 -0300

    Update test.py

--- test.py
+++ test.py
@@ -7,20 +7,42 @@ def test_n_Negativo():
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    assert data['n'] == "Solo enteros positivos."
+    assert data['respuestaPhi'] == "Solo enteros positivos."
+    assert data['respuestaSuma'] == "Solo enteros positivos."
+    assert data['respuestaMult'] == "Solo enteros positivos."
 
 def test_n_No_Numerico():
     response = app.test_client().get('/phi/aaa')
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    assert data['n'] == "Solo enteros positivos."
+    assert data['respuestaPhi'] == "Solo enteros positivos."
+    assert data['respuestaSuma'] == "Solo enteros positivos."
+    assert data['respuestaMult'] == "Solo enteros positivos."
 
 def test_n_Positivo():
     response = app.test_client().get('/phi/3')
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    assert data['n'] == 2
+    assert data['respuestaPhi'] == 2
+    assert data['respuestaSuma'] == 6
+    assert data['respuestaMult'] == 14
 
-    
+def test_n_Positivo_mas():
+    response = app.test_client().get('/phi/7')
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['respuestaPhi'] == 13
+    assert data['respuestaSuma'] == 28
+    assert data['respuestaMult'] == 140
+
+def test_n_Positivo_mas_aun():
+    response = app.test_client().get('/phi/9')
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['respuestaPhi'] == 34
+    assert data['respuestaSuma'] == 45
+    assert data['respuestaMult'] == 285
